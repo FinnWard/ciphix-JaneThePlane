5135430ba978c81240b915e79a131f908ddfa1b8
FinnWard <33222605+FinnWard@users.noreply.github.com>
2020-10-18 17:56:22 +0200

    now weather report is being passed back to chatbot need to have more error handeling

--- src/intents/weather.ts
+++ src/intents/weather.ts
@@ -22,19 +22,20 @@ export const weather = (conv: any) => {
       console.log('Date: ' + date);
     }
 
-    //let path = '/data/2.5/find' + '?q=' + encodeURIComponent(city) + '&appid=' + ApiKey;
-    
 
-    return conv.add(
-        callWeatherApi(city,date)
-    )
+    //let path = '/data/2.5/find' + '?q=' + encodeURIComponent(city) + '&appid=' + ApiKey;
+    return callWeatherApi(city, date).then((output) => {
+        conv.add(output); // Return the results of the weather API to Dialogflow
+      }).catch(() => conv.add(`I don't know the weather but I hope it's good!`));
+
+
     }
 
     
     function callWeatherApi (city: string, date: string) { //voorbeeld functie van https://github.com/dialogflow/fulfillment-weather-nodejs/blob/master/functions/index.js
         return new Promise((resolve, reject) => {
         // Create the path for the HTTP request to get the weather
-        let path = '/data/2.5/find' + '?q=' + encodeURIComponent(city) + '&appid=' + ApiKey + '&units=metric';
+        let path = '/data/2.5/find' + '?q=' + encodeURIComponent(city) + '&appid=' + ApiKey + '&units=metric&lang=nl';
         console.log('API Request: ' + host + path);
   
       // Make the HTTP request to get the weather
@@ -49,9 +50,9 @@ export const weather = (conv: any) => {
           let conditions = response['list'][0]['weather'][0];
   
           // Create response
-          let output = `Current conditions in ${location} 
-          are ${conditions['main']} with a projected high of
-          ${forecast['temp_max']}°C and a low of 
+          let output = `op het moment in ${location} 
+          is het ${conditions['description']} met een hoog van
+          ${forecast['temp_max']}°C en een laag van 
           ${forecast['temp_min']}°C.`;
   
           // Resolve the promise with the output text
@@ -65,42 +66,4 @@ export const weather = (conv: any) => {
       });
     });
 };
-
-/*
-function callWeatherApi (city: string, date: string) { //voorbeeld functie van https://github.com/dialogflow/fulfillment-weather-nodejs/blob/master/functions/index.js
-    return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the weather
-    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-        '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
-    console.log('API Request: ' + host + path);
-
-  // Make the HTTP request to get the weather
-  http.get({host: host, path: path}, (res) => {
-    let body = ''; // var to store the response chunks
-    res.on('data', (d) => { body += d; }); // store each response chunk
-    res.on('end', () => {
-      // After all the data has been received parse the JSON for desired data
-      let response = JSON.parse(body);
-      let forecast = response['data']['weather'][0];
-      let location = response['data']['request'][0];
-      let conditions = response['data']['current_condition'][0];
-      let currentConditions = conditions['weatherDesc'][0]['value'];
-
-      // Create response
-      let output = `Current conditions in the ${location['type']} 
-      ${location['query']} are ${currentConditions} with a projected high of
-      ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
-      ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
-      ${forecast['date']}.`;
-
-      // Resolve the promise with the output text
-      console.log(output);
-      resolve(output);
-    });
-    res.on('error', (error) => {
-      console.log(`Error calling the weather API: ${error}`)
-      reject();
-    });
-  });
-});
-};*/
+//test
