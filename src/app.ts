09c9858228b477d0f41bf24311845eb1e4488f30
FinnWard <33222605+FinnWard@users.noreply.github.com>
2020-10-18 21:34:36 +0200

    added error handeling to avoid a crash when no city was passed.

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import { WebhookClient } from 'dialogflow-fulfillment'
 import { fallback } from './intents/fallback'
 import { welcome } from './intents/welcome'
 import { weather } from './intents/weather'
+import { welcomeWeather } from './intents/welcomeWeather'
+import { airport } from './intents/airport'
 
 const app = express()
 
@@ -23,7 +25,9 @@ const intents = new Map<string, (agent: any) => void>()
 // Set specific intent-name to it's respective method
 intents.set('Default Fallback Intent', fallback)
 intents.set('Default Welcome Intent', welcome)
+intents.set('Default Welcome Intent - weer', welcomeWeather)
 intents.set('Huidige weer', weather)
+intents.set('Airports', airport)
 
 
 app.post('/', async (req, res) => {
@@ -31,5 +35,5 @@ app.post('/', async (req, res) => {
 
     await agent.handleRequest(intents)
 })
-
+//
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}!`))
--- src/intents/weather.ts
+++ src/intents/weather.ts
@@ -1,4 +1,3 @@
-import { request, response } from "express";
 import * as http from 'http';
 
 //const host = 'api.worldweatheronline.com';
@@ -13,7 +12,10 @@ const ApiKey = '483ddfa2413a8028b5b145c7c93c2775';
 export const weather = (conv: any) => {
 
     //required paramater to get weather info
-    let city = conv.parameters.location['city']; 
+    let city = conv.parameters.location['city'];
+    if (!city){
+        return conv.add('Hier is het wel lekker weer! Waar zit jij?')
+    }
 
     //parsing a date if available.
     let date = '';
@@ -22,48 +24,45 @@ export const weather = (conv: any) => {
       console.log('Date: ' + date);
     }
 
-
-    //let path = '/data/2.5/find' + '?q=' + encodeURIComponent(city) + '&appid=' + ApiKey;
     return callWeatherApi(city, date).then((output) => {
         conv.add(output); // Return the results of the weather API to Dialogflow
-      }).catch(() => conv.add(`I don't know the weather but I hope it's good!`));
+    }).catch(() => conv.add(`I don't know the weather but I hope it's good!`));
 
 
-    }
+}
 
-    
-    function callWeatherApi (city: string, date: string) { //voorbeeld functie van https://github.com/dialogflow/fulfillment-weather-nodejs/blob/master/functions/index.js
-        return new Promise((resolve, reject) => {
+function callWeatherApi (city: string, date: string) { //voorbeeld functie van https://github.com/dialogflow/fulfillment-weather-nodejs/blob/master/functions/index.js
+    return new Promise((resolve, reject) => {
         // Create the path for the HTTP request to get the weather
         let path = '/data/2.5/find' + '?q=' + encodeURIComponent(city) + '&appid=' + ApiKey + '&units=metric&lang=nl';
         console.log('API Request: ' + host + path);
   
-      // Make the HTTP request to get the weather
-      http.get({host: host, path: path}, (res) => {
-        let body = ''; // var to store the response chunks
-        res.on('data', (d) => { body += d; }); // store each response chunk
-        res.on('end', () => {
-          // After all the data has been received parse the JSON for desired data
-          let response = JSON.parse(body);
-          let forecast = response['list'][0]['main'];
-          let location = response['list'][0]['name'];
-          let conditions = response['list'][0]['weather'][0];
+        // Make the HTTP request to get the weather
+        http.get({host: host, path: path}, (res) => {
+            let body = ''; // var to store the response chunks
+            res.on('data', (d) => { body += d; }); // store each response chunk
+            res.on('end', () => {
+            // After all the data has been received parse the JSON for desired data
+            let response = JSON.parse(body);
+            let forecast = response['list'][0]['main'];
+            let location = response['list'][0]['name'];
+            let conditions = response['list'][0]['weather'][0];
+    
+            // Create response
+            let output = `op het moment in ${location} 
+            is het ${conditions['description']} met een hoog van
+            ${forecast['temp_max']}°C en een laag van 
+            ${forecast['temp_min']}°C.`;
   
-          // Create response
-          let output = `op het moment in ${location} 
-          is het ${conditions['description']} met een hoog van
-          ${forecast['temp_max']}°C en een laag van 
-          ${forecast['temp_min']}°C.`;
-  
-          // Resolve the promise with the output text
-          console.log(output);
-          resolve(output);
+            // Resolve the promise with the output text
+            console.log(output);
+            resolve(output);
         });
         res.on('error', (error) => {
-          console.log(`Error calling the weather API: ${error}`)
-          reject();
+            console.log(`Error calling the weather API: ${error}`)
+            reject();
         });
       });
     });
 };
-//test
+//test  
